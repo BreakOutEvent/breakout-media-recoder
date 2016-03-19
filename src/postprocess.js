7b16fb61498032af918fbe38353f7ba522b0e921
Philipp Piwowarsky <piwo.mail@googlemail.com>
2016-03-19 13:01:20 +0100

    postProcess result validation #1

--- src/postprocess.js
+++ src/postprocess.js
@@ -20,7 +20,11 @@ var log = bunyan.createLogger({
 module.exports = function (id, file, type, size) {
     if (config.postprocess) {
         azureclient.uploadFile(file).then(function (res) {
-            postRequest(id, file, res.url, type);
+            postRequest(id, file, res.url, type).then(function (msg) {
+                console.log(msg)
+            }).catch(function (e) {
+                console.error("ERROR", e);
+            });
             console.log(id, size.name, res.url);
             log.info({id: id, orig: path.parse(file).base, remotename: res.remotename, url: res.url, type, size})
         });
@@ -29,26 +33,38 @@ module.exports = function (id, file, type, size) {
 
 function postRequest(id, file, itemurl, type) {
 
-    var token = jsonwebtoken.sign({subject: id.toString()}, config.posthook.jwt_secret, {algorithm: 'HS512'});
+    return new Promise(function (resolve, reject) {
 
-    helpers.getMetaData(file).then(function (thissize) {
-        request({
-            method: 'post',
-            body: {
-                url: itemurl,
-                width: thissize.width,
-                height: thissize.height,
-                length: thissize.length,
-                size: thissize.size,
-                type: type
-            },
-            headers: {
-                'X-UPLOAD-TOKEN': token
-            },
-            json: true,
-            url: `${config.posthook.url}${id}/`
-        }, function (err, res, body) {
-            if (err) console.error(err, body);
+        var token = jsonwebtoken.sign({subject: id.toString()}, config.posthook.jwt_secret, {algorithm: 'HS512'});
+        helpers.getMetaData(file).then(function (thissize) {
+            request({
+                method: 'post',
+                body: {
+                    url: itemurl,
+                    width: thissize.width,
+                    height: thissize.height,
+                    length: thissize.length,
+                    size: thissize.size,
+                    type: type
+                },
+                headers: {
+                    'X-UPLOAD-TOKEN': token
+                },
+                json: true,
+                url: `${config.posthook.url}${id}/`
+            }, function (err, res, body) {
+                if (err) {
+                    console.error(err, body);
+                    reject(err);
+                }
+                if (res.statusCode === 201) {
+                    resolve("backend accepted");
+                } else {
+                    reject(`status: ${res.statusCode}`);
+                }
+            });
+        }).catch(function (e) {
+            reject(`Metadata Error! ${e}`);
         });
     });
 }
